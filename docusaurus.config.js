8e90d7ad2dfbec151f15ad6b09f28c4da3cf7ea3
0xtomm <84812080+0xtomm@users.noreply.github.com>
2024-11-10 12:38:59 +0000

    fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/citrus-finance/citrus-docs/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/citrus-finance/citrus-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/citrus-finance/citrus-docs/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
